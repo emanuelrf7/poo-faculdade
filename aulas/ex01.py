12e3b5d44fa82174f33d5a5a85f77286bc941eac
Emanuel Rosa Franco <139822686+emanuelrf7@users.noreply.github.com>
2024-09-14 11:24:53 -0300

    Update ex01.py

--- aulas/ex01.py
+++ aulas/ex01.py
@@ -1,71 +1,113 @@
+# Classe Carro:
+#
+# Atributos: marca, modelo, ano, velocidade_atual, estado
+# Métodos: acelerar, frear, ligar, desligar
+# •	acelerar(quantidade): aumenta a velocidade atual do
+# carro pela quantidade especificada.
+# •	frear(quantidade): diminui a velocidade atual do
+# carro pela quantidade especificada, sem deixar que a velocidade fique negativa.
+# •	ligar(): altera o estado do carro para ligado.
+# •	desligar(): altera o estado do carro para desligado e
+# zera a velocidade atual.
+
+class Marca:
+
+    def _init_(self):
+        self.nome = ""
+
+    def getNome(self):
+        return self.nome
+
+    def setNome(self, nome):
+        self.nome = nome
+
+class Modelo:
+
+    def _init_(self):
+        self.nome = ""
+
+    def getNome(self):
+        return self.nome
+
+    def setNome(self, nome):
+        self.nome = nome
+
 class Carro:
-    def __init__(self):
-        self.marca = ""
-        self.modelo = ""
+    def _init_(self):
+        self.marca = None
+        self.modelo = None
         self.ano = 0
-        self.velocidade_atual = 0.0
-        self.estado = ""
-
-    def set_marca(self, marca):
-        self.marca = marca
-
+        self.velocidade_atual = 0
+        self.estado = "desligado"
     def get_marca(self):
         return self.marca
-
-    def set_modelo(self, modelo):
-        self.modelo = modelo
+    def set_marca(self, marca):
+        self.marca = marca
 
     def get_modelo(self):
         return self.modelo
 
-    def set_ano(self, ano):
-        self.ano = ano
-
-    def get_ano(self):
-        return self.ano
-
-    def set_velocidade(self, velocidade_atual):
-        self.velocidade_atual = velocidade_atual
-
-    def get_velocidade(self):
-        return self.velocidade_atual
-
-    def set_estado(self, estado):
-        self.estado = estado
+    def set_modelo(self, modelo):
+        self.modelo = modelo
 
     def get_estado(self):
         return self.estado
 
+    def set_estado(self, estado):
+        self.estado = estado
+
+    def get_velocidade_atual(self):
+        return self.velocidade_atual
+
+    def set_velocidade_atual(self, velocidade_atual):
+        if velocidade_atual < 0:
+            print("velocidade invalida")
+        else:
+            self.velocidade_atual = velocidade_atual
     def ligar(self):
-        self.estado = "ligado"
-
+        if self.estado == "desligado":
+            self.estado = "ligado"
+        else:
+            print("carro já ligado")
     def desligar(self):
-        self.estado = "desligado"
-
-    def acelerar(self, quantidade):
         if self.estado == "ligado":
-            self.velocidade_atual = self.velocidade_atual + quantidade
+            self.estado = "desligado"
+            self.velocidade_atual = 0
+        else:
+            print("carro já desligado")
+    def getNomeMarca(self):
+        if self.marca == None:
+            print("Carro está sem marca")
+        else:
+            return self.marca.getNome()
 
-    def frear(self, quantidade):
-        if self.estado == "ligado":
-            self.velocidade_atual = self.velocidade_atual - quantidade
+    def getNomeModelo(self):
+        if self.modelo == None:
+            print("Carro está sem modeo")
+        else:
+            return self.modelo.getNome()
 
-
+marca = Marca()
+marca.setNome("Fiat")
+modelo = Modelo()
+modelo.setNome("Marea")
 carro = Carro()
-carro.set_marca("Jeep")
-print(carro.get_marca())
-carro.set_modelo("Compass")
-print(carro.get_modelo())
-carro.set_ano(2024)
-print(carro.get_ano())
-carro.set_velocidade(40.5)
-print(carro.get_velocidade())
-carro.set_estado("ligado")
+carro.set_marca(marca)
+carro.set_modelo(modelo)
+print(carro.getNomeMarca())
+print(carro.getNomeModelo())
 print(carro.get_estado())
-carro.frear(30.5)
-print(carro.get_velocidade())
-carro.frear(10)
-print(carro.get_estado())
-print(carro.get_velocidade())
-carro.desligar()
-print(carro.get_estado())
+
+# carro2 = Carro()
+# carro2.set_marca("Peugeot")
+# carro2.set_modelo("206")
+# carro2.ligar()
+# carro2.ligar()
+# print(carro2.get_marca())
+# print(carro2.get_estado())
+# carro2.set_velocidade_atual(100)
+# print(carro2.get_velocidade_atual())
+# carro2.desligar()
+# carro2.desligar()
+# print(carro2.get_estado())
+# print(carro2.get_velocidade_atual())
