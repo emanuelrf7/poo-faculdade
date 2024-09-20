896c1dbb28c019933cc2c00a6c5ffd3f9a79dda7
Manel <emanuelrf8@gmail.com>
2024-09-20 10:02:23 -0300

    Atualizei alguns exercícios

--- aulas/ex02.py
+++ aulas/ex02.py
@@ -4,42 +4,72 @@
 # Métodos: depositar, sacar
 # •	depositar(valor): adiciona o valor ao saldo da conta.
 # •	sacar(valor): subtrai o valor do saldo da conta, se houver saldo suficiente.
-class ContaBancaria:
+class Titular:
     def __init__(self):
         self.titular = ""
+    
+
+    def getTitular(self):
+        return self.titular
+    
+
+    def setTitular(self, titular):
+        self.titular = titular
+
+
+class ContaBancaria:
+    def __init__(self):
+        self.titular = None
         self.numero_conta = 0.0
         self.saldo = 0.0
     
+
     def set_titular(self, titular):
         self.titular = titular
     
+
     def get_titular(self):
         return self.titular
     
+
     def set_numero_conta(self, numero_conta):
         self.numero_conta = numero_conta
     
+
     def get_numero_conta(self):
         return self.numero_conta
     
+
     def set_saldo(self, saldo):
         self.saldo = saldo
     
+
     def get_saldo(self):
         return self.saldo
     
+
     def depositar(self, valor):
         self.saldo += valor
     
+
     def sacar(self, valor):
         if self.saldo >= self.saldo:
             self.saldo -= valor
         else:
             print("Saldo insuficiente!")
     
+
+    def getNomeTitular(self):
+        if self.titular == None:
+            print("Titular está sem nome")
+        else:
+            return self.titular.getTitular()
+
+titular = Titular()
+titular.setTitular("Emanuel")    
 conta_bancaria = ContaBancaria()
-conta_bancaria.set_titular('Emanuel')
-print(conta_bancaria.get_titular())
+conta_bancaria.set_titular(titular)
+print(conta_bancaria.getNomeTitular())
 conta_bancaria.set_saldo(700)
 print(conta_bancaria.get_saldo())
 conta_bancaria.depositar(300)
--- aulas/ex03.py
+++ aulas/ex03.py
@@ -7,67 +7,123 @@
 # •	marcar_pagina(pagina): Marca a página atual do livro.
 # •	avancar_pagina(): Avança uma página, se não estiver na última página.
 # •	retroceder_pagina(): Retrocede uma página, se não estiver na primeira página.
-class Livro:
+class Titulo:
     def __init__(self):
         self.titulo = ""
+    
+
+    def getTitulo(self):
+        return self.titulo
+    
+
+    def setTitulo(self, titulo):
+        self.titulo = titulo
+
+
+class Autor:
+    def __init__(self):
         self.autor = ""
+    
+
+    def getAutor(self):
+        return self.autor
+    
+
+    def setAutor(self, autor):
+        self.autor = autor
+
+
+class Genero:
+    def __init__(self):
+        self.genero = ""
+    
+
+    def getGenero(self):
+        return self.genero
+    
+
+    def setGenero(self, genero):
+        self.genero = genero
+
+
+class Livro:
+    def __init__(self):
+        self.titulo = None
+        self.autor = None
         self.ano_publicacao = 0
         self.numero_paginas = 0
-        self.genero = ""
+        self.genero = None
         self.pagina = 0
+    
     
     def set_titulo(self, titulo):
         self.titulo = titulo
     
+
     def get_titulo(self):
         return self.titulo
     
+
     def set_autor(self, autor):
         self.autor = autor
     
+
     def get_autor(self):
         return self.autor
     
+
     def set_ano_publicacao(self, ano_publicacao):
         self.ano_publicacao = ano_publicacao
     
+
     def get_ano_publicacao(self):
         return self.ano_publicacao
     
+
     def set_numero_paginas(self, numero_paginas):
         self.numero_paginas = numero_paginas
     
+
     def get_numero_paginas(self):
         return self.numero_paginas
     
+
     def set_genero(self, genero):
         self.genero = genero
     
+
     def get_genero(self):
         return self.genero
     
+
     def set_pagina(self, pagina):
         self.pagina = pagina
     
+
     def get_pagina(self):
         return self.pagina
     
+
     def abrir(self):
         return "O livro foi aberto!"        
-        
+
+
     def fechar(self):
         return "O livro foi fechado!"
     
+
     def marcar_pagina(self):
         # self.pagina = pagina
         return print(f'Você marcou a página {self.pagina}')
     
+
     def avancar_pagina(self):
         if self.pagina <= self.numero_paginas:
             self.pagina += 1
         
         return print(f"Você avançou para a página {self.pagina}")
     
+
     def retroceder_pagina(self):
         if self.pagina <= self.numero_paginas:
             self.pagina -= 1
@@ -75,17 +131,44 @@ class Livro:
         return print(f"Você retornou para a página {self.pagina}")
 
 
+    def getNomeTitulo(self):
+        if self.titulo == None:
+            print("Titulo está sem nome")
+        else:
+            return self.titulo.getTitulo()
+    
+
+    def getNomeAutor(self):
+        if self.autor == None:
+            print("Autor está sem nome")
+        else:
+            return self.autor.getAutor()
+    
+
+    def getNomeGenero(self):
+        if self.genero == None:
+            print("Genero está sem nome")
+        else:
+            return self.genero.getGenero()
+
+
+titulo = Titulo()
+titulo.setTitulo("Ideias que rimam mais que palavras vol. 1")
+autor = Autor()
+autor.setAutor("Rashid")
+genero = Genero()
+genero.setGenero("Biografia")
 livro = Livro()
-livro.set_titulo("Ideias que rimam mais que palavras vol. 1")
-print(livro.get_titulo())
-livro.set_autor("Rashid")
-print(livro.get_autor())
+livro.set_titulo(titulo)
+print(livro.getNomeTitulo())
+livro.set_autor(autor)
+print(livro.getNomeAutor())
 livro.set_ano_publicacao(2018)
 print(livro.get_ano_publicacao())
 livro.set_numero_paginas(300)
 print(livro.get_numero_paginas())
-livro.set_genero("Biografia")
-print(livro.get_genero())
+livro.set_genero(genero)
+print(livro.getNomeGenero())
 print(livro.abrir())
 livro.set_pagina(50)
 livro.marcar_pagina()
