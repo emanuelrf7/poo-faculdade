e520cb52ab991d11cc7bb2b01532922b87965ca9
Manel <emanuelrf8@gmail.com>
2024-10-03 21:41:54 -0300

    Update exrelampago.py

--- aulas/exrelampago.py
+++ aulas/exrelampago.py
@@ -15,6 +15,7 @@ class Funcionario:
         self.setEscolaridade(escolaridade)
         self.escolaridade = escolaridade
         self.alocacao = None
+        self.filial = None
     
     
     def getEscolaridade(self):
@@ -35,9 +36,21 @@ class Funcionario:
     def set_alocacao(self, alocacao):
         self.alocacao = alocacao
     
+
+    def get_filial(self):
+        return self.filial
+    
+
+    def set_filial(self, filial):
+        self.filial = filial
+    
     
     def getNomeEscolaridade(self):
         return self.escolaridade.getNome()
+    
+
+    def getNomeEstado(self):
+        return self.filial.getNomeEstado()
 
 class Grupo:
     def __init__(self):
@@ -92,11 +105,84 @@ class Departamento:
     
     
     def set_chefia(self, chefia):
-        self.chefia = chefia    
+        self.chefia = chefia
+
+class Estado:
+    def __init__(self):
+        self.nome = ""
+
+
+    def get_nome(self):
+        return self.nome
+    
+
+    def set_nome(self, nome):
+        self.nome = nome
+
+class Cidade:
+    def __init__(self):
+        self.cidade = ""
+        self.estado = None
+    
+
+    def get_cidade(self):
+        return self.cidade
+    
+
+    def set_cidade(self, cidade):
+        self.cidade = cidade
+    
+
+    def get_estado(self, estado):
+        self.estado = estado
+
+    
+    def set_estado(self, estado):
+        self.estado = estado
+    
+
+    def getNomeEstado(self):
+        return self.estado.get_nome()
+
+class Filial:
+    def __init__(self):
+        self.filial = ""
+        self.cidade = None
+    
+
+    def get_filial(self):
+        return self.filial
+    
+
+    def set_filial(self, filial):
+        self.filial = filial
+    
+
+    def get_cidade(self):
+        return self.cidade
+    
+
+    def set_cidade(self, cidade):
+        self.cidade = cidade
+    
+
+    def getNomeEstado(self):
+        return self.cidade.getNomeEstado()
         
+estado = Estado()
+estado.set_nome("Rio de Janeiro")
+cidade = Cidade()
+cidade.set_estado(estado)
+# print(cidade.getNomeEstado())
+filial = Filial()
+filial.set_cidade(cidade)
+print(filial.getNomeEstado())
 grupo = Grupo()
 escolaridade = Escolaridade()
 escolaridade.setNome("Doutorado")
 presidente = Funcionario(escolaridade)
 grupo.setPresidente(presidente)
 print(grupo.getNomeEscolaridadePresidente())
+funcionario = Funcionario()
+funcionario.set_filial(filial)
+print(funcionario.getNomeEstado())
